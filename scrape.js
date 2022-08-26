78ec3e17b90c870550b2418791a9585d2e2714ef
sandeep <sandgup3@cisco.com>
2022-08-26 12:28:53 +0530

    deploy

--- scrape.js
+++ scrape.js
@@ -1,7 +1,9 @@
 const puppeteer = require("puppeteer");
 
 async function scrape(url, test) {
-  const browser = await puppeteer.launch({ devtools: false });
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
 
   await page.goto(url);
