95a6477a327b793fb81992976b02fdcb5d622c03
sandeep <sandgup3@cisco.com>
2022-08-26 12:25:10 +0530

    deploy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const {scrape} = require("./scrape");
 const app = express();
-const port = 3000;
+const port = process.env.port || process.env.PORT || 3000;
 
 app.get("/", async (req, res) => {
   console.clear();
